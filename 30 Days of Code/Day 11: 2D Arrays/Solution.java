e12325e451e43e5ba3983e31d98f2de5d8a3ad5a
KM Zubair <47664890+KM-Zubair@users.noreply.github.com>
2020-01-22 00:33:02 +0800

    Create Solution.java

---
+++ 30 Days of Code/Day 11: 2D Arrays/Solution.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+        
+        
+        for (int i =0; i < 6; i++) {
+        	
+        	String[] arrRowItems = scanner.nextLine().split(" ");
+        	scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        	
+        	for (int j =0; j < 6; j++) {
+        		int arrItem = Integer.parseInt(arrRowItems[j]);
+        		arr[i][j] = arrItem;
+        	}
+        }
+        
+        int max = Integer.MIN_VALUE ;
+        for (int i = 0; i < 4; i++) {  
+        //i is taken 4 so it doesnt occur out of bound exception
+        	
+        	for (int j = 0; j < 4; j++) {
+        		
+        		int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] +
+        			  arr[i+1][j+1] +
+        			  arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+        		
+        		if (sum > max ) {
+            		max = sum;
+            	}
+        	}
+        	
+        }
+        
+        System.out.println(max);
+        scanner.close();
+    }
+}
+
+
+
+
+
